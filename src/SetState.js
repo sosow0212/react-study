a5647e5dc25cc95737bcff1a4039054a0046bb30
sosow0212 <sosow0212@naver.com>
2021-09-10 16:44:00 +0900

    Learn Using SetState

---
+++ src/SetState.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { useState } from "react";
+
+function SetState() {
+  
+  const [Number, setNumber] = useState(0)
+
+  const increaseNum = () => {
+    setNumber(Number+1);
+  }
+
+  const decreaseNum = () => {
+    setNumber(Number-1);
+  }
+
+  return (
+    <div>
+      <h1>Using setState</h1>
+      <h2>Number : {Number} </h2>
+      <button onClick={increaseNum}>Increase Number</button>
+      <button onClick={decreaseNum}>Decrease Number</button>
+
+    </div>
+  );
+}
+
+export default SetState;

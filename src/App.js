f8f9fbc4a24620d6c4c5926679ac443c003a103b
sosow0212 <sosow0212@naver.com>
2021-09-10 17:36:50 +0900

    Learn : props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import SetState from "./SetState";
-import UseState2 from "./UseState2";
+import SetState from "./components/SetState";
+import UseState2 from "./components/UseState2";
+import Rendering from "./components/UseEffect";
+import UseEffect from "./components/UseEffect";
 
 function App() {
+
+  const text = "props 테스트";
+
+  const obj = {
+    name: "yoon",
+    age: 12
+  }
+
   return (
     <div className="App">
-      <SetState />
-      <UseState2 />
+      {/* <SetState />
+      <UseState2 /> */}
+      <UseEffect name={obj.name}/>
     </div>
   );
 }
